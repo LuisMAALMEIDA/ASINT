9f52a49f15b7f92d39a6156ce79bb8ffd48aabae
Unknown <diogonunes95@gmail.com>
2018-11-28 11:44:46 +0000

    Added building class

---
+++ Projecto/Code/Building.py
@@ -0,0 +1,7 @@
+class Building:
+	def __init__(self, id, name, latitude, longitude, radius):
+		self.id = id
+		self.name = name
+		self.latitude = latitude
+		self.longitude = longitude
+		self.radius = radius
