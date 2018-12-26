77050acb71d7b5c8f98e69113a7a83869e9cc62d
Luís Almeida <luis.mig.almeida96@gmail.com>
2018-12-26 12:01:45 +0000

    Send and Receive messages (Messages queues)

--- Projecto/Code/User.py
+++ Projecto/Code/User.py
@@ -1,9 +1,13 @@
+from math import sin, cos, sqrt, atan2, radians
+
 class User:
 
     def __init__(self, istid, name, range):
         self.istid = istid
         self.name = name
         self.range = range
+        self.latitude = float(0.0) # Atual latitude of the user
+        self.longitude = float(0.0) # Atual longitude of the user
         self.LoggedIn = True
 
     def toDict(self):
@@ -16,6 +20,48 @@ class User:
         else:
             return False
 
+    # Define a range for a specific user
     def DefineRange(self, range):
         self.range = range
+
+    # It verifies if this user is on the range of User X
+    def InRangeOfAnotherUser(self, X_latitude, X_longitude, X_range):
+
+        # If the user is not logged then he don't deserve to receive any message
+        if( self.LoggedIn == False):
+            return False
+
+        # Approximate radius of earth in km
+        R = 6373.0
+
+        # User own latitude and longitude
+        lat1 = radians(self.latitude)
+        lon1 = radians(self.longitude)
+
+        # Received latitude and longitude
+        lat2 = radians(X_latitude)
+        lon2 = radians(X_longitude)
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = R * c # in Km
+        distanceInMeters = distance*1000 # in meters  
+        print(self.istid)
+        print(self.latitude)
+        print(self.longitude)
+        print("range") 
+        print(X_range)
+        print("distancia")
+        print(distanceInMeters)
+        print("\n\n")
+
+        if (float(X_range) - float(distanceInMeters)  >= 0 ):
+            return True
+        else:
+            return False
+        
 
--- Projecto/Code/app.py
+++ Projecto/Code/app.py
@@ -4,24 +4,34 @@ from flask import request
 from flask import jsonify
 from flask import redirect
 from flask import make_response
-
 from Building import Building
 from User import User
 import memcache
 import requests
+import pika # Queues from RabbitMQ
 
-from werkzeug.contrib.cache import SimpleCache
+from werkzeug.contrib.cache import SimpleCache # Import cache for the access_token
 c = SimpleCache()       
 
+# Establish a connection with RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
 
 app = Flask(__name__)
 
-
+messages = []
 buildings =  []
 users = []
 logs = []
 
-
+# PARA ELIMINAR (SO PARA TESTAR)
+u = User("ist181882", "Juliaqueen", 8)
+u.latitude = 39.236668699999996
+u.longitude = -8.6859944
+users.append(u)
+messages.append(u)
+channel.queue_declare(queue = "ist181882")
+#############################################
 
 def AuthenticateAdmin(username, password):
     if username == "admin" and password == "123":
@@ -47,9 +57,13 @@ def CreateBuilding():
     print(name)
     print(latitude)
     print(longitude)
+
     # Add the building to the vector of buildings
     b1= Building( id, name, latitude, longitude, radius)
     buildings.append(b1)
+
+    
+
     return jsonify(str({"Msg" : "Builing created"}))
 
 # List all buildings on the server
@@ -137,13 +151,23 @@ def FenixAuthentication():
         response.headers['Expires'] = '3600'
         response.set_cookie('user_id', value = user_istid , max_age=3600 )
         response.set_cookie('access_token', value=access_token, max_age=3600 )
-
+        response.set_cookie('name', value=user_name, max_age=3600 )
+        
         # Set cache for the server
-        print(user_istid)
+        
         c.set(user_istid, access_token, timeout=3600)
         key= c.get(user_istid)
         print(key)
- 
+        
+        # TODO ISTO È PARA POR NA BASE DE DADOS (que são os users)
+        if(not(any(user.istid == user_istid for user in users))):
+                U = User(user_istid, user_name, 0)
+                users.append(U)                
+
+
+        # Create message queues for each user (i.e when the user does login)
+        channel.queue_declare(queue = user_istid)
+
         return response
         #return redirect("static/mainPage.xhtml")
 
@@ -162,8 +186,31 @@ def SendMessageToNearbyUsers(UserId):
     content = request.json
     message = content['Message']
     print(message)
-    print(UserId)
-    # TODO  Send message to everyone that is one user range
+
+    # Gets the actual latitude, longitude and range of the sending user
+    sendingUser = next(filter(lambda x: x.istid == UserId,  users))
+    la = float(sendingUser.latitude)
+    lo = float(sendingUser.longitude)
+    r = float(sendingUser.range)
+
+    # Filters the nearby users that are in user rangeU
+    usersInRange = list(filter(lambda x: x.InRangeOfAnotherUser(la, lo, r), users))
+
+    # Remove the user who sent the message from list of nearby users
+    usersInRange1 = list(filter(lambda x: str(x.istid) != UserId, usersInRange))
+
+
+    # Put the message in the users' "mail box" queue message
+    for user in usersInRange1:
+        print("HELLO MODAFUCKER")
+        print(str(user.istid))
+        print(message)
+        channel.basic_publish(exchange='',
+                      routing_key=str(user.istid),
+                      body=message)
+
+
+
     return jsonify("Message sent")
  
 
@@ -171,17 +218,17 @@ def SendMessageToNearbyUsers(UserId):
 @app.route('/API/Users/DefineRange/<UserId>', methods = ['POST'])
 def DefineRange(UserId):
 
-    # TODO   
+    # TODO   FALTA POR ISTO NA BASE DE DADOS
     # Retrive the range from the webpage in meters
     content = request.json
     range = content['Range']
-    print (range)
-    '''for user in users:
+
+    for user in users:
         if(user.istid == UserId ):
             user.DefineRange(range)
-            break'''
+            break
 
-    return jsonify("Range defined for"+ str(range) +" meters")
+    return jsonify("Range defined for "+ str(range) +" meters")
 
 # List the nearby useres: on the range of the messages and on
 # the same building
@@ -189,30 +236,70 @@ def DefineRange(UserId):
 def ListNearbyUsers(UserId):
     # TODO
     # Dummies values
-    users.append(User(81232, "luisalmeida", 5))
-    users.append(User(81108, "diogonunes", 5))
-    NearbyUsers = list(map(lambda x: x.toDict(), users) )
-    print (NearbyUsers)
-    return jsonify(NearbyUsers)
+    #users.append(User(81232, "luisalmeida", 5))
+    #users.append(User(81108, "diogonunes", 5))
+
+    # Gets the actual latitude, longitude and range of the sending user
+    sendingUser = next(filter(lambda x: x.istid == UserId,  users))
+    la = float(sendingUser.latitude)
+    lo = float(sendingUser.longitude)
+    r = float(sendingUser.range)
+
+    # Filters the nearby users that are in user rangeU
+    usersInRange = list(filter(lambda x: x.InRangeOfAnotherUser(la, lo, r), users))
+
+    # Remove the user who sent the message from list of nearby users
+    usersInRange1 = list(filter(lambda x: str(x.istid) != UserId, usersInRange))
+
+    NearbyUsersList = list(map(lambda x: x.toDict(), usersInRange1) )
+    print (NearbyUsersList)
+    return jsonify(NearbyUsersList)
+
+
+
+
 
 #Send the messages to the user sent by other users and bots
 @app.route('/API/Users/ReceiveMessages/<UserId>')
 def ReceiveMessages(UserId):
     # TODO
     messages = []
-    messages.append({"Message": "something beautifull"})
-    messages.append({"Message": "something ugly"})
+
+    #messages.append({"Message": "something beautifull"})
+    #messages.append({"Message": "something ugly"})
+    queue_state = channel.queue_declare(queue=UserId, durable=True, passive=True)
+    queue_empty = queue_state.method.message_count == 0
+
+    while(not(queue_empty)):
+        queue_state = channel.queue_declare(queue=UserId, durable=True, passive=True)
+        queue_empty = queue_state.method.message_count == 0
+	
+        if not queue_empty:
+                method, properties, body = channel.basic_get(queue=UserId, no_ack=True)  
+                messages.append({"Message" : body.decode('UTF-8')})
+                print(body.decode('UTF-8'))
+    
+    print(messages)
     msg= list(messages)
     return jsonify(msg) 
 
 # Update location of the user
 @app.route('/API/Users/UpdatePosition/<UserId>', methods = ['POST'] )
 def UpdatePosition(UserId):
-    # TODO
 
-    content = request.json
-    latitude = content['Latitude']
-    print (latitude)
+    # Retrives the information about the user in order to update his location
+    for user in users:
+            if (user.istid == UserId):
+                content = request.json
+                Latitude = content['Latitude']
+                Longitude = content['Longitude']
+                user.latitude = float(Latitude)
+                user.longitude = float(Longitude)
+
+
+    # TODO Falta fazer a cena dos logs, isto é comparar se o user está a entrar
+    # num building ou a  sair
+
     return jsonify("happy")
 
 ############### Bots endpoints ####################
@@ -226,4 +313,4 @@ def BotSendMessages(BuildingId):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
